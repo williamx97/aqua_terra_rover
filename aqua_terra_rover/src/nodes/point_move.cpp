74144e53ffa2c264711c95cc13715fcac68a2284
William <williamx@student.unimelb.edu.au>
2021-05-22 20:05:10 +1000

    test

--- aqua_terra_rover/src/nodes/point_move.cpp
+++ aqua_terra_rover/src/nodes/point_move.cpp
@@ -69,6 +69,9 @@ int main(int argc, char* argv[])
 		double roll, pitch, odom_heading;
 		m.getRPY(roll, pitch, odom_heading);
 		
+        ROS_INFO_STREAM("[POINT MOVE] x direction is " << odom_x);
+        ROS_INFO_STREAM("[POINT MOVE] y direction is " << odom_y);
+        ROS_INFO_STREAM("[POINT MOVE] theta direction is " << odom_heading);
         /*ERROR CALCULATIONS START********************************************************************************************/
         //Calculate how far away the current [x,y] is from the goal [x,y] (Distance Between 2 points)
         error_distance = sqrt( pow(goal_x-odom_x,2) + pow(goal_y-odom_y,2) );
