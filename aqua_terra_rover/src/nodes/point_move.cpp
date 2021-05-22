14335e867875a252943dbc87e038098392a7f520
William <williamx@student.unimelb.edu.au>
2021-05-22 20:49:13 +1000

    ADS

--- aqua_terra_rover/src/nodes/point_move.cpp
+++ aqua_terra_rover/src/nodes/point_move.cpp
@@ -69,9 +69,9 @@ int main(int argc, char* argv[])
 		double roll, pitch, odom_heading;
 		m.getRPY(roll, pitch, odom_heading);
 		
-        // ROS_INFO_STREAM("[POINT MOVE] x direction is " << odom_x);
-        // ROS_INFO_STREAM("[POINT MOVE] y direction is " << odom_y);
-        // ROS_INFO_STREAM("[POINT MOVE] theta direction is " << odom_heading);
+        ROS_INFO_STREAM("[POINT MOVE] x direction is " << odom_x);
+        ROS_INFO_STREAM("[POINT MOVE] y direction is " << odom_y);
+        ROS_INFO_STREAM("[POINT MOVE] theta direction is " << odom_heading);
 
         /*ERROR CALCULATIONS START********************************************************************************************/
         //Calculate how far away the current [x,y] is from the goal [x,y] (Distance Between 2 points)
@@ -147,8 +147,8 @@ int main(int argc, char* argv[])
                 output_v = 0;
         }
 
-        ROS_INFO_STREAM("[POINT MOVE] Moving with [v] " << output_v);
-        ROS_INFO_STREAM("[POINT MOVE] Moving with [w] " << output_w);
+        // ROS_INFO_STREAM("[POINT MOVE] Moving with [v] " << output_v);
+        // ROS_INFO_STREAM("[POINT MOVE] Moving with [w] " << output_w);
         //Send the velocity command to motor controllers
         geometry_msgs::Twist cmd_out;
         cmd_out.linear.x = output_v;
